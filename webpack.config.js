6aed358973fb0cc767007b44e05d67470caf7b29
Tim Siwula <tim.siwula@consensys.net>
2018-04-05 03:13:07 -0700

    fixed webpack bugs

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,20 @@ var config = {
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: __dirname + '/build'
   },
   devtool: 'source-map',
   devServer: {
-    inline: true,
     contentBase: __dirname + '/build',
+    inline: true,
+    // disableHostCheck: true,
+    host: '0.0.0.0',
     port: 3080
   },
   module: {
     loaders: [{
       test: /\.jsx?/,
-      include: __dirname,
+      include: __dirname + '/src',
       loader: 'babel',
       query: {presets: ['es2015', 'react']}
       }]
