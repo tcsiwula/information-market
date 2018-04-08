c2b58cc8f87d3f1b7117ceb0744af5dc93316024
Tim Siwula <tim.siwula@consensys.net>
2018-04-08 15:12:01 -0700

    configure env vars for prod stag dev

--- README.md
+++ README.md
@@ -21,3 +21,14 @@ and enter queries
   hi
 }
 ```
+# npm scripts:
+```
+"scripts": {
+  "development": "NODE_ENV=development PORT=3333 nodemon --exec babel-node server.js",
+  "staging": "NODE_ENV=staging PORT=9999 forever --exec babel-node server.js",
+  "production": "NODE_ENV=production PORT=3080 forever start server.js",
+  "start": "nodemon --exec babel-node server.js",
+  "react-development": "webpack-dev-server --color",
+  "clean": "rm -rf build && mkdir build"
+},
+```
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
-import typeDefs from './schema.js';
+import typeDefs from './schema';
 import resolvers from './resolvers';
 import models from './src/models';
+const path = require('path');
+var env = process.env.NODE_ENV || 'development';
+const dir = (path.join(__dirname, "./config.json"));
+const config = require(dir)[env];
 // Put together a schema
 const schema = makeExecutableSchema({
 	typeDefs,
@@ -18,6 +22,7 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 // sync() will create all table if they doesn't exist in database
 models.sequelize.sync().then(() => {
-	app.listen(8080);
-	console.log('Go to http://0.0.0.0:8080/graphiql to run queries!');
+	console.log('************  config.port = ' + config.port);
+	app.listen(config.port);
+	console.log('GraphQL server running at http://0.0.0.0:' + config.port + '/graphiql');
 });
